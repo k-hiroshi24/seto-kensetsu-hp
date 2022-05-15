f98782b0ac9ab03be60b7a9d9417faec598e24a6
hiroshi <h.kawaguchi0204@gmail.com>
2022-05-15 23:30:57 +0900

    各セクションの修正など

--- src/js/script.js
+++ src/js/script.js
@@ -110,41 +110,41 @@ var swiper2 = new Swiper(".mySwiper2", {
 
 
 // service-tab
-(() => {
-  const $doc = document;
-  const $tab = $doc.getElementById('js-tab');
-  const $nav = $tab.querySelectorAll('[data-nav]');
-  const $content = $tab.querySelectorAll('[data-content]');
-  // console.log('$content', $content);
-  // 初期化
-  const init = () => {
-    $content[0].style.display = 'block'
-  };
-  init();
-  // クリックしたら実行されるイベント
-  const handleClick = (e) => {
-    e.preventDefault();
+// (() => {
+//   const $doc = document;
+//   const $tab = $doc.getElementById('js-tab');
+//   const $nav = $tab.querySelectorAll('[data-nav]');
+//   const $content = $tab.querySelectorAll('[data-content]');
+//   // console.log('$content', $content);
+//   // 初期化
+//   const init = () => {
+//     $content[0].style.display = 'block'
+//   };
+//   init();
+//   // クリックしたら実行されるイベント
+//   const handleClick = (e) => {
+//     e.preventDefault();
     
-    // クリックされたされたnavとそのdataを取得
-    const $this = e.target
-    const targetVal = $this.dataset.nav;
-    // console.log('$targetVal', $targetVal);
-    // 対象外のnav,contentを全て一旦リセットする
-    let index = 0
-    while(index < $nav.length){
-      $content[index].style.display = 'none';
-      $nav[index].classList.remove('is-active');
-      index++
-    }
+//     // クリックされたされたnavとそのdataを取得
+//     const $this = e.target
+//     const targetVal = $this.dataset.nav;
+//     // console.log('$targetVal', $targetVal);
+//     // 対象外のnav,contentを全て一旦リセットする
+//     let index = 0
+//     while(index < $nav.length){
+//       $content[index].style.display = 'none';
+//       $nav[index].classList.remove('is-active');
+//       index++
+//     }
 
-    // 対象のコンテンツをアクティブ化する
-    $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
-    $nav[targetVal].classList.add('is-active');
-  };
-// 全nav要素に対して関数を適応・発火
-  let index = 0;
-  while(index < $nav.length) {
-    $nav[index].addEventListener('click', (e) => handleClick(e));
-    index++;
-  }
-})();
+//     // 対象のコンテンツをアクティブ化する
+//     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+//     $nav[targetVal].classList.add('is-active');
+//   };
+// // 全nav要素に対して関数を適応・発火
+//   let index = 0;
+//   while(index < $nav.length) {
+//     $nav[index].addEventListener('click', (e) => handleClick(e));
+//     index++;
+//   }
+// })();
